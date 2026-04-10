303bf95c96f0ae143cec9a628bf03bf8555cdea5
Francesco Doria <doria5@live.it>
2026-04-10 17:20:09 +0200

    add username init

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,39 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { db } from '#/db/initDb'
+import { useForm } from '@tanstack/react-form'
 
 export const Route = createFileRoute('/')({ component: App })
 
 function App() {
+  const form = useForm({
+    defaultValues: {
+      username: '',
+    },
+    onSubmit: async ({ value }) => {
+      console.log(value)
+    },
+  })
+
   return (
     <main className="">
-      hello world
+      <form
+        onSubmit={(e) => {
+          e.stopPropagation()
+          e.preventDefault()
+          form.handleSubmit()
+        }}
+      >
+        <form.Field
+          name='username'
+          children={(field) => {
+            return (
+              <input type='text' name='username' id='username' onChange={(e) => {
+                field.handleChange(e.target.value)
+              }} />
+            )
+          }}
+        ></form.Field>
+      </form>
     </main>
   )
 }
